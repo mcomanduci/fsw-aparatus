2586f5cab7f12ba8dad050e6cbf8d0bae4bd4e28
Marcelo <mcomanduci@gmail.com>
2025-11-09 17:37:45 -0300

    final

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,18 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+  globalForPrisma.prisma ||
+  new PrismaClient({ adapter }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
