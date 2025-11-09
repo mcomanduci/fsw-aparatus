66ab00cf747a3ecc083f9e3fc580cc67c21d44e4
Marcelo <mcomanduci@gmail.com>
2025-11-09 17:30:57 -0300

    final

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,9 +5,7 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const prisma =
+export const prisma =
   globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
