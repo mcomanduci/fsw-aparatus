2ffab5110c533e2fc4123ea5c4af212f47e38fa0
Marcelo <mcomanduci@gmail.com>
2025-11-09 17:41:26 -0300

    final

--- app/generated/prisma/client.ts
+++ app/generated/prisma/client.ts
@@ -14,7 +14,7 @@ import * as path from 'node:path'
 import { fileURLToPath } from 'node:url'
 globalThis['__dirname'] = path.dirname(fileURLToPath(import.meta.url))
 
-import * as runtime from "@prisma/client/runtime/library"
+import * as runtime from "@prisma/client/runtime/client"
 import * as $Enums from "./enums"
 import * as $Class from "./internal/class"
 import * as Prisma from "./internal/prismaNamespace"
@@ -39,9 +39,6 @@ export type PrismaClient<LogOpts extends Prisma.LogLevel = never, OmitOpts exten
 export { Prisma }
 
 
-// file annotations for bundling tools to include these files
-path.join(__dirname, "query_engine-windows.dll.node")
-path.join(process.cwd(), "app/generated/prisma/query_engine-windows.dll.node")
 
 /**
  * Model Barbershop
